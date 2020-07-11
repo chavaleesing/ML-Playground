f12ecb9a6d6689e504ff389fbac264be08698f43
chavaleesing <chavalee.sing@gmail.com>
2020-07-11 16:40:34 +0700

    add comment to knn

--- ml-algorithm/knn.py
+++ ml-algorithm/knn.py
@@ -7,15 +7,15 @@ default_path = os.path.abspath(os.getcwd())
 
 path = f"{default_path}/dataset/fruit.csv"
 data_frames = pd.read_csv(path, delimiter=",,")
-print(data_frames.head())
+# print(data_frames.head())
 
 X = data_frames[['height', 'width', 'mass', 'color_score']]
 y = data_frames['fruit_label']
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 
 knn = KNeighborsClassifier(n_neighbors = 5)
-knn.fit(X_train, y_train)
-knn.score(X_test, y_test)
+knn.fit(X_train, y_train) # training data
+knn.score(X_test, y_test) # evaluating knn model
 
 fruit_prediction = knn.predict([[100, 6.3, 6.8, 0.55]])
 print(fruit_prediction)
