5fb9582cd10fe05eb6aa0304f4a8a0c314f2aa3d
chavaleesing <chavalee.sing@gmail.com>
2020-07-13 11:01:34 +0700

    Update KNN algorithm

---
+++ ml-algorithm/knn/regressor.py
@@ -0,0 +1,36 @@
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import numpy as np
+
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.abspath(os.curdir)))
+from dataset.dataset_generator import create_simple_regression_dataset
+
+
+X, y = create_simple_regression_dataset(plot=True)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 0)
+
+knnreg = KNeighborsRegressor(n_neighbors = 5).fit(X_train, y_train)
+
+print(knnreg.predict(X_test))
+print('R-squared test score: {:.3f}'
+     .format(knnreg.score(X_test, y_test)))
+
+fig, subaxes = plt.subplots(1, 2, figsize=(8,4))
+X_predict_input = np.linspace(-3, 3, 50).reshape(-1,1)
+X_train, X_test, y_train, y_test = train_test_split(X[0::5], y[0::5], random_state = 0)
+
+for thisaxis, K in zip(subaxes, [1, 3]):
+    knnreg = KNeighborsRegressor(n_neighbors = K).fit(X_train, y_train)
+    y_predict_output = knnreg.predict(X_predict_input)
+    thisaxis.set_xlim([-2.5, 0.75])
+    thisaxis.plot(X_predict_input, y_predict_output, '^', markersize = 10,
+                 label='Predicted', alpha=0.8)
+    thisaxis.plot(X_train, y_train, 'o', label='True Value', alpha=0.8)
+    thisaxis.set_xlabel('Input feature')
+    thisaxis.set_ylabel('Target value')
+    thisaxis.set_title('KNN regression (K={})'.format(K))
+    thisaxis.legend()
+plt.tight_layout()
