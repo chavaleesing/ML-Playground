3ad2719c484d122a747f52bf2e273c9d47dee2e6
chavaleesing <chavalee.sing@gmail.com>
2020-07-20 18:24:50 +0700

    rename floder to services

--- ml-algorithm/classification/naive_bayes.py
+++
@@ -1,6 +0,0 @@
-from sklearn.naive_bayes import GaussianNB
-from adspy_shared_utilities import plot_class_regions_for_classifier
-
-X_train, X_test, y_train, y_test = train_test_split(X_C2, y_C2, random_state=0)
-
-nbclf = GaussianNB().fit(X_train, y_train)
