57f93dbce71309343e7eeba49702feabb3240942
chavaleesing <chavalee.sing@gmail.com>
2020-07-20 18:40:30 +0700

    config vscode for run debugger

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ML-Playground
 Machine Learning playground using Python
+
+1. `pipenv install`
+2. `uvicorn main:app --reload` 
+    or run with **DeBug** via vscode
--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from routers import classification, regression
+import uvicorn
+
 
 app = FastAPI()
 
 app.include_router(classification.router, prefix="/classification")
 app.include_router(regression.router, prefix="/regression")
+
+
+if __name__ == "__main__":
+    import os
+    import sys
+    ROOT_DIR = os.path.abspath(os.curdir)
+    sys.path.append(ROOT_DIR)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
