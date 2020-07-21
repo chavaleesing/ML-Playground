49c5d65599d8af48ecf1017bc2cb1711c4e52009
chavaleesing <chavalee.sing@gmail.com>
2020-07-21 20:33:28 +0700

    add knn regression method

--- routers/regression.py
+++ routers/regression.py
@@ -1,13 +1,25 @@
 from fastapi import APIRouter
+from services.regression import knn as knn_service
+import os
 
+from fastapi import APIRouter, Request
+from starlette.responses import HTMLResponse
+ROOT_DIR = os.path.abspath(os.curdir)
 router = APIRouter()
-
 
 @router.get("/")
 async def test():
-    return [{"classification": "200 OK"}]
+    return [{"regression": "200 OK"}]
 
 
-@router.get("/linear")
-async def decision_tree():
-    return {"decisiontree": "decision tree"}
+@router.get("/knn")
+async def knn(params: Request = {}):
+    response = knn_service.demonstrate(params.query_params)
+    if params.query_params.get("plot"):
+        response = HTMLResponse(
+        f'<html> \
+            <h2><pre>{response}</pre></h2> \
+            <img src="{ROOT_DIR}/temp_plt.png" alt="plt"/> \
+        </html>'
+        )
+    return response
--- services/regression/knn.py
+++ services/regression/knn.py
@@ -1,36 +1,33 @@
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsRegressor
 
-import os
-import sys
-sys.path.append(os.path.abspath(os.path.abspath(os.curdir)))
 from utils.dataset_generator import create_simple_regression_dataset
 
 
-X, y = create_simple_regression_dataset()
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 0)
+def demonstrate(params):
+    X, y = create_simple_regression_dataset()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 0)
+    reg = KNeighborsRegressor(n_neighbors = 3).fit(X_train, y_train)
 
-knnreg = KNeighborsRegressor(n_neighbors = 5).fit(X_train, y_train)
+    accuracy = {
+        "accuracy_train_set": reg.score(X_train, y_train),
+        "accuracy_test_set": reg.score(X_test, y_test)
+    }
 
-print(knnreg.predict(X_test))
-print('R-squared test score: {:.3f}'
-     .format(knnreg.score(X_test, y_test)))
+    if params.get("plot"):
+        fig, subaxes = plt.subplots(1, 1, figsize=(8,4))
+        X_predict_input = np.linspace(-3, 3, 50).reshape(-1,1)
+        y_predict_output = reg.predict(X_predict_input)
+        subaxes.plot(X_predict_input, y_predict_output, '^', markersize = 10,
+                    label='Predicted', alpha=0.8)
+        subaxes.plot(X_train, y_train, 'o', label='True Value', alpha=0.8)
+        subaxes.set_xlabel('Input feature')
+        subaxes.set_ylabel('Target value')
+        subaxes.set_title('KNN regression (K={3})')
+        subaxes.legend()
+        plt.tight_layout()
+        fig.savefig('temp_plt.png', bbox_inches='tight')
 
-fig, subaxes = plt.subplots(1, 2, figsize=(8,4))
-X_predict_input = np.linspace(-3, 3, 50).reshape(-1,1)
-X_train, X_test, y_train, y_test = train_test_split(X[0::5], y[0::5], random_state = 0)
-
-for thisaxis, K in zip(subaxes, [1, 3]):
-    knnreg = KNeighborsRegressor(n_neighbors = K).fit(X_train, y_train)
-    y_predict_output = knnreg.predict(X_predict_input)
-    thisaxis.set_xlim([-2.5, 0.75])
-    thisaxis.plot(X_predict_input, y_predict_output, '^', markersize = 10,
-                 label='Predicted', alpha=0.8)
-    thisaxis.plot(X_train, y_train, 'o', label='True Value', alpha=0.8)
-    thisaxis.set_xlabel('Input feature')
-    thisaxis.set_ylabel('Target value')
-    thisaxis.set_title('KNN regression (K={})'.format(K))
-    thisaxis.legend()
-plt.tight_layout()
+    return accuracy
